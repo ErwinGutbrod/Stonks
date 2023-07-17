bfe880dc4dfc83e545cdbf9589d4a2fcb08ecf37
ErwinGutbrod <erwingutbrod@gmail.com>
2023-07-17 11:28:19 -0300

    [Master] EG: First push

---
+++ Stonks.Core/Services/IChatBotService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stonks.Model.Services
+{
+    public interface IChatBotService
+    {
+    }
+}
---
+++ Stonks.Model/Entities/ChatRoom.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using Stonks.Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Web;
+
+namespace Stonks.Model
+{
+    public class ChatRoom
+    {
+        Guid ChatRoomId { get; set; }
+
+        public string ChatRoomName { get; set; }
+        
+        public string ChatRoomDescription { get; set;}
+
+        public List<Message> Messages { get; set;}
+
+        public IdentityUser Owner { get; set; }
+
+        public List<IdentityUser> Members { get; set; }
+    
+    }
+}
